3be412b36a72469fa703ffd84632a234209f0484
Aktindo <71206933+Aktindo@users.noreply.github.com>
2021-02-13 20:40:46 +0530

    badges

--- commands/information/userinfo.js
+++ commands/information/userinfo.js
@@ -26,6 +26,27 @@ module.exports = {
                                     return "This user's status is not cached in the bot's data";
             }
         }
+
+        const BADGES = {
+            "DISCORD_EMPLOYEE": "<:DiscordStaff:810164713065283694>",
+            "DISCORD_PARTNER": "<:DiscordPartner:810164035319496725>",
+            "BUGHUNTER_LEVEL_1": "<:BugHunter:810164035273228290>",
+            "HYPESQUAD_EVENTS": "<:HypeSquadEvents:810164035034284072>",
+            "HOUSE_BRAVERY": "<:HypeSquadBravery:810164036452483112>",
+            "HOUSE_BRILLIANCE": "<:HypeSquadBrilliance:810164035231809577>",
+            "HOUSE_BALANCE": "<:HypeSquadBalance:810164035022487623>",
+            "EARLY_SUPPORTER": "<:EarlySupporter:810164033461420052>",
+            "VERIFIED_BOT": "<:Verified:810164034983952454>",
+            "VERIFIED_DEVELOPER": "<:Verified:810164034983952454>"
+        };
+
+        let userFlags;
+        if (member.user.flags === null) {
+            userFlags = ""
+        } else {
+            userFlags = member.user.flags.toArray();
+        }
+
         const messageCountRes = await messageCountModel.findOne({
             guildId: message.guild.id,
             userId: member.id,
@@ -41,6 +62,7 @@ module.exports = {
         const userinfo = new MessageEmbed()
         .setAuthor(member.user.username, member.user.displayAvatarURL({dynamic: true}))
         .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
+        .addField('Badges', userFlags.length ? userFlags.map(flag => BADGES[flag]).join("") : "None", false)
         .addField('Registered', `${moment(member.user.createdAt).format('LLLL')}\n(${moment(member.user.createdAt).startOf('minutes').fromNow()})`, true)
         .addField('Joined', `${moment(member.joinedAt).format('LLLL')}\n(${moment(member.joinedAt).startOf('minutes').fromNow()})`, true)
         .addField('Status', `${userStatus()}`, false)
