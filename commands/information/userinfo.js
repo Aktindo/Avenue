93bae331cd6b3ec498345ae73c7ac62099fdc093
Aktindo <71206933+Aktindo@users.noreply.github.com>
2021-02-14 11:21:13 +0530

    userinfo command

--- commands/information/userinfo.js
+++ commands/information/userinfo.js
@@ -12,6 +12,13 @@ module.exports = {
     async execute(client, message, args) {
         const moment = require("moment");
         let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member
+
+        if (member.user.bot) {
+            return member.channel.send(
+                client.embedError(message, 'Cannot fetch information about bots.')
+            )
+        }
+
         const userStatus = () => {
             switch (member.user.presence.status) {
                 case "online":
@@ -25,6 +32,15 @@ module.exports = {
                                 default:
                                     return "This user's status is not cached in the bot's data";
             }
+        }
+
+        const trimArray = (arr, maxLen = 10) => {
+            if (arr.length > maxLen) {
+                const len = arr.length - maxLen;
+                arr = arr.slice(0, maxLen);
+                arr.push(` and ${len} more...`);
+            }
+            return arr;
         }
 
         const BADGES = {
@@ -56,9 +72,10 @@ module.exports = {
         else {
             messageCount = messageCountRes.messageCount
         }
-        let memberRoles
-        if (member.roles.cache.size > 15) memberRoles = "Too many roles to display"
-        else memberRoles = member.roles.cache.map(r => r).join(', ')
+        const roles = member.roles.cache
+            .sort((a, b) => b.position - a.position)
+            .map(role => role.toString())
+            .slice(0, -1);
         const userinfo = new MessageEmbed()
         .setAuthor(member.user.username, member.user.displayAvatarURL({dynamic: true}))
         .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
@@ -67,7 +84,7 @@ module.exports = {
         .addField('Joined', `${moment(member.joinedAt).format('LLLL')}\n(${moment(member.joinedAt).startOf('minutes').fromNow()})`, true)
         .addField('Status', `${userStatus()}`, false)
         .addField('Total Messages', messageCount, true)
-        .addField(`Roles[${member.roles.cache.size}]`, memberRoles, false)
+        .addField(`Roles [${member.roles.cache.size}]`, roles.length < 10 ? roles.join(", ") : roles.length > 10 ? trimArray(roles).join(", ") : "None", false)
         .setColor('BLURPLE')
         message.channel.send(userinfo)
     }
